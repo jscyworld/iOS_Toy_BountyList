576e48186596f8809fb496d7c4d4b70ba5566bd8
SIRIUS KIM <jmoumnet@gmail.com>
2023-11-16 16:28:38 +0900

    231116 ToyProject - BountyList
    1. Custom cell을 클릭했을 때 보여줄 DetailViewController 생성
    2. Present modally segue로 BountyView와 DetailView 연결

---
+++ BountyList/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by JONG SOO KIM on 11/16/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func btnCloseAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
