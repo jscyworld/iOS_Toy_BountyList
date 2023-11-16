217039df22c2ba45832c7a2c486bcb7f60a14449
SIRIUS KIM <jmoumnet@gmail.com>
2023-11-16 15:25:34 +0900

    Initial Commit

---
+++ BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by JONG SOO KIM on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
